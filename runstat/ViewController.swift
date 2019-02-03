f1c9562f2ae47c55e4694588fb12e862eb6aa337
Linus Löfgren <linus.lofgren@viscando.com>
2019-02-03 16:49:57 +0100

    Initial Commit

---
+++ runstat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  runstat
+//
+//  Created by Linus Löfgren on 2019-02-03.
+//  Copyright © 2019 Linus Löfgren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
